cc62de7bd27151b211ca30c4fa93cf4efd0b22b1
leijia <905257522@qq.com>
2019-04-20 08:05:43 +0800

    '20-1'

--- Five/src/store/modules/user.js
+++ Five/src/store/modules/user.js
@@ -3,102 +3,97 @@ import { getToken, setToken, removeToken } from '@/utils/auth'
 import router, { resetRouter } from '@/router'
 
 const state = {
-    token: getToken(),
-    name: '',
-    avatar: '',
-    introduction: '',
-    roles: [],
-    userInfo: {}
+  token: getToken(),
+  name: '',
+  avatar: '',
+  introduction: '',
+  roles: [],
+  userInfo: {}
 }
 
 const mutations = {
-    SET_TOKEN: (state, token) => {
-        state.token = token
-    },
-    SET_INTRODUCTION: (state, introduction) => {
-        state.introduction = introduction
-    },
-    SET_NAME: (state, name) => {
-        state.name = name
-    },
-    SET_AVATAR: (state, avatar) => {
-        state.avatar = avatar
-    },
-    SET_ROLES: (state, roles) => {
-        state.roles = roles
-    },
-    SET_USERINFO: (state, userInfo) => {
-        state.userInfo = userInfo
-    }
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_INTRODUCTION: (state, introduction) => {
+    state.introduction = introduction
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  },
+  SET_ROLES: (state, roles) => {
+    state.roles = roles
+  },
+  SET_USERINFO: (state, userInfo) => {
+    state.userInfo = userInfo
+  }
 }
 
 const actions = {
-    // user login
-    async login({ commit }, userInfo) {
-        const { username, password } = userInfo
-        var res = await login({ user_name: username, user_pwd: password })
-        setToken(res.token)
-        return res
-    },
+  // user login
+  async login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    var res = await login({ user_name: username, user_pwd: password })
+    setToken(res.token)
+    return res
+  },
+  // get user info
+  async getInfo({ commit, state }) {
+    const data = await getInfo()
+    commit('SET_USERINFO', data.data)
+    return data.data
+  },
 
-    // get user info
-    async getInfo({ commit, state }) {
-        const data = await getInfo()
-        commit('SET_USERINFO', data.data)
-        return data.data
-    },
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token).then(() => {
+        commit('SET_TOKEN', '')
+        commit('SET_ROLES', [])
+        removeToken()
+        resetRouter()
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
 
-    // user logout
-    logout({ commit, state }) {
-        return new Promise((resolve, reject) => {
-            logout(state.token).then(() => {
-                commit('SET_TOKEN', '')
-                commit('SET_ROLES', [])
-                removeToken()
-                resetRouter()
-                resolve()
-            }).catch(error => {
-                reject(error)
-            })
-        })
-    },
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      removeToken()
+      resolve()
+    })
+  },
 
-    // remove token
-    resetToken({ commit }) {
-        return new Promise(resolve => {
-            commit('SET_TOKEN', '')
-            commit('SET_ROLES', [])
-            removeToken()
-            resolve()
-        })
-    },
+  // Dynamically modify permissions
+  changeRoles({ commit, dispatch }, role) {
+	return new Promise(async resolve => {
+      const token = role + '-token'
+      commit('SET_TOKEN', token)
+      setToken(token)
 
-    // Dynamically modify permissions
-    changeRoles({ commit, dispatch }, role) {
-        return new Promise(async resolve => {
-            const token = role + '-token'
+      const { roles } = await dispatch('getInfo')
+      resetRouter()
 
-            commit('SET_TOKEN', token)
-            setToken(token)
-
-            const { roles } = await dispatch('getInfo')
-
-            resetRouter()
-
-            // generate accessible routes map based on roles
-            const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
-
-            // dynamically add accessible routes
-            router.addRoutes(accessRoutes)
-
-            resolve()
-        })
-    }
+      // generate accessible routes map based on roles
+      const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
+      // dynamically add accessible routes
+      router.addRoutes(accessRoutes)
+      resolve()
+    })
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
