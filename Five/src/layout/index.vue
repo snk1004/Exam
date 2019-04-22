7c5044dcf293503906a4a07f67761dea78ae01a6
Zhaoqian <953884785@qq.com>
2019-04-22 13:35:31 +0800

    4221

--- Five/src/layout/index.vue
+++ Five/src/layout/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div :class="classObj" class="app-wrapper">
+  <div class="app">
+    <header>
+      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551624718911&di=4a7004f8d71bd8da84d4eadf1b59e689&imgtype=0&src=http%3A%2F%2Fimg105.job1001.com%2Fupload%2Falbum%2F2014-10-15%2F1413365052_95IE3msH.jpg" alt="">
+      
+    </header>
+    <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
     <div :class="{hasTagsView:needTagsView}" class="main-container">
@@ -9,6 +14,7 @@
       </div>
       <app-main />
     </div>
+  </div>
   </div>
 </template>
 
@@ -57,7 +63,17 @@ export default {
 <style lang="scss" scoped>
   @import "~@/styles/mixin.scss";
   @import "~@/styles/variables.scss";
-
+header{
+  width: 100%;
+  height: 64px;
+  padding: 0 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  >img{
+    width: 120px;
+  }
+}
   .app-wrapper {
     @include clearfix;
     position: relative;
@@ -66,7 +82,7 @@ export default {
 
     &.mobile.openSidebar {
       position: fixed;
-      top: 0;
+      top: 64;
     }
   }
 
